815175312143f2d4eacfc0dff03337d6d0b04abd
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-05-21 23:06:38 -0400

    final 6KYU answer submitted on codeward

--- stop-gninnips-my-sdrow.js
+++ stop-gninnips-my-sdrow.js
@@ -2,9 +2,42 @@
 
 // Examples: spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw" spinWords( "This is a test") => returns "This is a test" spinWords( "This is another test" )=> returns "This is rehtona test"
 
-function spinWords() {
-  //TODO Have fun :)
-  //has to take in a string of one or more words but with five or more letter works reversed
-  //split reverse join
-  //map > for loop
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// MY ATTEMPT
+// MISSING FOR LOOP IN FIRST ATTEMPT
+
+// function spinWords(str){
+//   let splitStr = str.split(' ')
+//   // let reverseStr = splitStr.reverse()
+//   // let joinStr = reverseStr.join('')
+//   if(splitStr.length >= 5){
+//     str.reverse()
+//   }
+//   return splitStr
+// }
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// HELP FROM STACKOVERFLOW
+function spinWords(string) {
+  //splits string into words separated by a space
+  let splitStr = string.split(" ");
+  for (var i = 0; i < splitStr.length; i++) {
+    //if the word is more than 5 chars, reverse the word
+    if (splitStr[i].length >= 5) {
+      splitStr[i] = splitStr[i].split("").reverse().join("");
+    }
+  } //end for loop
+  //join the modified array
+  let joinStr = splitStr.join(" ");
+  return joinStr;
+}
+
+//BEST PRACTICE AND CLEVER ANSWER ON CODEWARS
+function spinWords(words) {
+  return words
+    .split(" ")
+    .map(function (word) {
+      return word.length > 4 ? word.split("").reverse().join("") : word;
+    })
+    .join(" ");
 }
