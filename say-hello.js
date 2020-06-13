341ad01ca1d0d8b0c07f6a93eca0c108bc062e96
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-06-12 23:33:55 -0400

    pushing final Code Wars answer

---
+++ say-hello.js
@@ -0,0 +1,23 @@
+// Say hello!
+
+// Write a function to greet a person. Function will take name as input and greet the person by saying hello. Return null/nil/None if input is empty string or null/nil/None.
+
+// Example:
+
+// greet("Niks") === "hello Niks!";
+// greet("") === null; // Return null if input is empty string
+// greet(null) === null; // Return null if input is null
+
+//FIRST ANSWER
+function greet(name) {
+  if (name === "") {
+    return null;
+  } else if (name === null) {
+    return null;
+  } else {
+    return `hello ${name}!`;
+  }
+}
+
+//FINAL ANSWER
+const greet = (name) => (name ? `hello ${name}!` : null);
