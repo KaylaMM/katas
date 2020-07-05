a4b7a8bb4c425c93bbeccf68f6912db7630c4bf8
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-07-04 23:39:32 -0400

    only part of problem solved

--- 6_KYUs/find-the-odd-init.js
+++ 6_KYUs/find-the-odd-init.js
@@ -1,3 +1,10 @@
 // Given an array, find the integer that appears an odd number of times.
 
 // There will always be only one integer that appears an odd number of times.
+
+function findOdd(A) {
+  let odds = A.filter(function (x) {
+    return x % 2 === 1;
+  });
+  console.log(odds);
+}
