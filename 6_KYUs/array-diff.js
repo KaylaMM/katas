3b413fe4a66dd5f5c73db277298dfb91925d76ce
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-07-11 08:44:40 -0400

    final code wars answer

--- 6_KYUs/array-diff.js
+++ 6_KYUs/array-diff.js
@@ -6,3 +6,13 @@
 // If a value is present in b, all of its occurrences must be removed from the other:
 
 // arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+function arrayDiff(a, b) {
+  if (a.length > 0 && b.length > 0) {
+    return (diff = a.filter((num) => !b.includes(num)));
+  } else if (b.length == 0) {
+    return a;
+  } else {
+    return a;
+  }
+}
