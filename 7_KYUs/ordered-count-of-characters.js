004325d7487289edd39f09d4121c15a7dcd13b6a
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-08-08 23:22:36 -0400

    don't want to lose this

--- 7_KYUs/ordered-count-of-characters.js
+++ 7_KYUs/ordered-count-of-characters.js
@@ -3,3 +3,21 @@
 // Example:
 
 // orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
+
+function orderedCount(text) {
+  let letterCounter = [];
+  let arr = text.split("").map((letter, i) => {
+    return !letterCounter[letter]
+      ? letterCounter[letter]
+      : letterCounter.push(i);
+  });
+  return arr;
+}
+
+// create a new array
+// create a counter
+// identify if the letter exists in the parent array
+// if not add it in a new array, push to parent
+// if yes +1 to child array in parent
+// retutn parent array
+//==>use two maps?
