93402b2fe331c0ad43f6fb0588d3de9c43b3f687
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-10-04 23:48:33 -0400

    saving work, final answer not submitted

---
+++ 7_KYUs/string-doubles.js
@@ -0,0 +1,25 @@
+// In this Kata, you will write a function doubles that will remove double string characters that are adjacent to each other.
+
+// For example:
+
+// doubles('abbcccdddda') = 'aca', because, from left to right:
+
+// a) There is only one 'a' on the left hand side, so it stays.
+// b) The 2 b's disappear because we are removing double characters that are adjacent. 
+// c) Of the 3 c's, we remove two. We are only removing doubles. 
+// d) The 4 d's all disappear, because we first remove the first double, and again we remove the second double.
+// e) There is only one 'a' at the end, so it stays.
+// Two more examples: doubles('abbbzz') = 'ab' and doubles('abba') = "". In the second example, when we remove the b's in 'abba', the double a that results is then removed.
+
+// The strings will contain lowercase letters only. More examples in the test cases.
+
+function doubles(s) {
+  let arr = s.split("");
+  for (i = 1; i < arr.length; i++) {
+    if (arr[i - 1] === arr[i]) {
+      arr.splice(i - 1, 1);
+      i = 1;
+    }
+  }
+  return arr.join;
+}
