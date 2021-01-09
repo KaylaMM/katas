acb0dfaf1659b3bb520f57f15a054e80f8d28854
Kayla Martinez <kaylamartinez1201@gmail.com>
2021-01-08 23:36:21 -0500

    still working on final answer

--- 8_KYUs/sum-of-differences-in-array.js
+++ 8_KYUs/sum-of-differences-in-array.js
@@ -10,3 +10,12 @@
 // Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
 
 // If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell).
+
+function sumOfDifferences(arr) {
+  //   return (arr.length >= 1) ? (arr[0] - arr[1]) + (arr[1] - arr[2]) : 0
+
+  let newArr = arr.map((x, i) => arr[i] - arr[i + 1]);
+  console.log(newArr);
+  let mapSum = newArr.map((x, i) => x + newArr[i + 1]);
+  return mapSum;
+}
