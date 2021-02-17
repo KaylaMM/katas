774e257bddaf7aa91b0b542ddd53ed381193f076
Kayla Martinez <kaylamartinez1201@gmail.com>
2021-02-16 22:37:40 -0500

    still working on it

--- 8_KYUs/bartender-drinks.js
+++ 8_KYUs/bartender-drinks.js
@@ -11,3 +11,22 @@
 // Note: anything else is the default case: if the input to the function is not any of the values in the table, then the return value should be "Beer".
 
 // Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
+
+function getDrinkByProfession(param) {
+  let table = [
+    { input: "Jabroni", output: "Patron Tequila" },
+    { input: "School Counselor", output: "Anything with Alcohol" },
+    { input: "Programmer", output: "Hipster Craft Beer" },
+    { input: "Bike Gang Member", output: "Moonshine" },
+    { input: "Politician", output: "Your tax dollars" },
+    { input: "Rapper", output: "Cristal" },
+  ];
+
+  let caps = param.toUpperCase();
+
+  return table.map((caps, input) => {
+    return table.output ? table.input === caps : "Beer";
+  });
+
+  return map;
+}
