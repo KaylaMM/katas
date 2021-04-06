984c8f3f108eb15df4c1b67f2c26f07e6791ba38
Kayla Martinez <kaylamartinez1201@gmail.com>
2021-04-06 17:35:21 -0400

    saving final answer

---
+++ 7_KYUs/homogenous-arrays.js
@@ -0,0 +1,27 @@
+// Challenge:
+
+// Given a two-dimensional array, return a new array which carries over only those arrays from the original, which were not empty and whose items are all of the same type (i.e. homogenous). For simplicity, the arrays inside the array will only contain characters and integers.
+
+// Example:
+
+// Given [[1, 5, 4], ['a', 3, 5], ['b'], [], ['1', 2, 3]], your function should return [[1, 5, 4], ['b']].
+
+// Addendum:
+
+// Please keep in mind that for this kata, we assume that empty arrays are not homogenous.
+
+// The resultant arrays should be in the order they were originally in and should not have its values changed.
+
+// No implicit type casting is allowed. A subarray [1, '2'] would be considered illegal and should be filtered out.
+
+function filterHomogenous(arrays) {
+  const arr = arrays.filter((value) =>
+    value.every((value, i, arr) => typeof value === typeof arr[0])
+  );
+
+  return arr.filter((value) => value.length !== 0);
+}
+
+// TOP ANSWER
+let filterHomogenous = (a) =>
+  a.filter((b) => b.length > 0 && b.every((e) => typeof e == typeof b[0]));
