c7c5573e140d2e844ee6a3bae282fbaf4a0dadaf
Kayla Martinez <kaylamartinez1201@gmail.com>
2021-04-12 23:13:58 -0400

    saving final answer

--- 7_KYUs/filter-the-number.js
+++ 7_KYUs/filter-the-number.js
@@ -6,3 +6,7 @@
 
 // Details
 // You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
+
+var FilterString = function (value) {
+  return parseInt(value.replace(/[a-z]/gi, ""));
+};
