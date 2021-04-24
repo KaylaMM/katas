bcf32ec86891cc262ed7f7f32a55639398c7b5ce
Kayla Martinez <kaylamartinez1201@gmail.com>
2021-04-23 22:18:56 -0400

    saving final answer

--- 7_KYUs/evens-times-last.js
+++ 7_KYUs/evens-times-last.js
@@ -3,12 +3,18 @@
 // If the sequence is empty, you should return 0.
 
 function evenLast(numbers) {
-  if (numbers.length === 0) {
-    return 0;
-  } else {
-    const evenNums = numbers
-      .filter((number) => number % 2 == 0)
-      .reduce((a, b) => a + b, 0);
-    return evenNums * numbers[numbers.length - 1];
-  }
+  if (numbers.length === 0) return 0;
+
+  const lastInt = numbers[numbers.length - 1];
+  const sum = numbers.reduce(
+    (acc, number, index) => (index % 2 === 0 ? acc + number : acc),
+    0
+  );
+  //  const evenNums = numbers.filter(number => number % 2 == 0).reduce((a, b) => a + b, 0)
+
+  return sum * lastInt;
 }
+
+const evenLast = (arr) =>
+  arr.filter((e, i) => i % 2 === 0).reduce((a, b) => a + b, 0) *
+    arr[arr.length - 1] || 0;
