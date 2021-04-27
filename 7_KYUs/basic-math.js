99a7d26901d07190508950add97411d902daa1cd
Kayla Martinez <kaylamartinez1201@gmail.com>
2021-04-26 23:55:17 -0400

    saving final answer

---
+++ 7_KYUs/basic-math.js
@@ -0,0 +1,18 @@
+// In this kata, you will do addition and subtraction on a given string. The return value must be also a string.
+
+// Note: the input will not be empty.
+
+// Examples
+// "1plus2plus3plus4"  --> "10"
+// "1plus2plus3minus4" -->  "2"
+
+function calculate(str) {
+  const splitArr = str.split("");
+  const numArr = splitArr.filter(Number).map(Number);
+
+  if (splitArr.includes("plus")) {
+    str.replace("plus", "+");
+  } else {
+    str.replace("minus", "-");
+  }
+}
