8fa9966de0c7860281423f90d7cb8a0a7375bad6
Kayla Martinez <kaylamartinez1201@gmail.com>
2021-05-16 23:04:59 -0400

    saving final answer

---
+++ 7_KYUs/small-enough.js
@@ -0,0 +1,12 @@
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+// You can assume all values in the array are numbers.
+
+function smallEnough(a, limit) {
+  return a.every((e) => (e <= limit ? true : false));
+}
+
+//BEST PRACTICE
+function smallEnough(a, limit) {
+  return Math.max(...a) <= limit;
+}
