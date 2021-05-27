de1dbd0fd47902409a8ec8b96dcee0dbe5d36321
Kayla Martinez <kaylamartinez1201@gmail.com>
2021-05-26 23:45:14 -0400

    still working on final answer

---
+++ 7_KYUs/can-they-code-same-language.js
@@ -0,0 +1,18 @@
+// Your task is to return either:
+
+// true if all developers in the list code in the same language; or
+// false otherwise.
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Daniel', lastName: 'J.', country: 'Aruba', continent: 'Americas', age: 42, language: 'JavaScript' },
+//   { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 22, language: 'JavaScript' },
+//   { firstName: 'Hanna', lastName: 'L.', country: 'Hungary', continent: 'Europe', age: 65, language: 'JavaScript' },
+// ];
+// your function should return true.
+
+function isSameLanguage(list) {
+  return list.map((obj, i) => {
+    return true ? obj.name[i] === obj.name[i + 1] : false;
+  });
+}
