1b2b6d931426a717255056dac54aae540e8adadc
Kayla Martinez <kaylamartinez1201@gmail.com>
2021-06-24 23:32:18 -0400

    saving final answer

--- 8_KYUs/stringy-strings.js
+++ 8_KYUs/stringy-strings.js
@@ -9,3 +9,15 @@
 // with size 12 should return : '101010101010'.
 
 // The size will always be positive and will only use whole numbers.
+
+function stringy(size) {
+  let result = "";
+  for (let i = 0; i < size; i++) {
+    if (i % 2 === 0) {
+      result += 1;
+    } else {
+      result += 0;
+    }
+  }
+  return result;
+}
