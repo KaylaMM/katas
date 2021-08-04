e8c9f6c8d2565763a20fa6f2ccb73f4378abc829
Kayla Martinez <kaylamartinez1201@gmail.com>
2021-08-03 23:47:46 -0400

    simple code wars

---
+++ 8_KYUs/remove-spaces.js
@@ -0,0 +1,10 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+function noSpace(x) {
+  return x.split(" ").join("");
+}
+
+//TOP ANSWER
+function noSpace(x) {
+  return x.replace(/\s/g, "");
+}
