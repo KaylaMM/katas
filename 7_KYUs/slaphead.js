d962073cf35adb5d18b001e552ff05effd24ffb9
Kayla Martinez <kaylamartinez1201@gmail.com>
2021-08-05 23:58:19 -0400

    saving final answer

--- 7_KYUs/slaphead.js
+++ 7_KYUs/slaphead.js
@@ -13,3 +13,33 @@
 // So for this head: "------/------" you shoud return:
 
 // ["-------------", "Unicorn"]
+
+// Final Answer
+function bald(x) {
+  let count = x.split("/").length - 1;
+  const z = Array(x.length).fill("-").join("");
+  if (count < 1) return [z, "Clean!"];
+  if (count < 2) return [z, "Unicorn!"];
+  if (count < 3) return [z, "Homer!"];
+  if (count < 6) return [z, "Careless!"];
+  return [z, "Hobo!"];
+}
+
+//Original Answer
+function bald(x) {
+  const arr = x.split("");
+  const count = arr.reduce((a, v) => (v === "/" ? a + 1 : a), 0);
+  const filtered = arr.filter((value, index, arr) => value != "/").join("");
+
+  if (count === 0) {
+    return [filtered, "Clean!"];
+  } else if (count === 1) {
+    return [filtered, "Unicorn!"];
+  } else if (count === 2) {
+    return [filtered, "Homer!"];
+  } else if (count > 2 || count === 5) {
+    return [filtered, "Careless!"];
+  } else {
+    return [filtered, "Hobo!"];
+  }
+}
