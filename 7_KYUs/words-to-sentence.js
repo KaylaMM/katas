b70ac7b0e7876d8980a672acc2c88f64cf3aaf05
Kayla Martinez <kaylamartinez1201@gmail.com>
2021-08-26 23:59:52 -0400

    not all changes were saved

---
+++ 7_KYUs/words-to-sentence.js
@@ -0,0 +1,9 @@
+// Write function which will create a string from a list of strings, separated by space.
+
+// Example:
+
+// ["hello", "world"] -> "hello world"
+
+function wordsToSentence(words) {
+  return words.join(" ");
+}
