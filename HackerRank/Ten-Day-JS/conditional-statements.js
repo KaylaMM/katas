1308ee13f2fcff2fa83e4bc37fd1fa9cf1e37a94
Kayla Martinez <kaylamartinez1201@gmail.com>
2021-09-25 23:50:01 -0400

    saving lesson

---
+++ HackerRank/Ten-Day-JS/conditional-statements.js
@@ -0,0 +1,21 @@
+function getLetter(s) {
+  switch (s) {
+    case 1:
+      "aeiou";
+      console.log("A");
+      break;
+    case 2:
+      "bcdfg";
+      console.log("B");
+      break;
+    case 3:
+      "hkjlm";
+      console.log("C");
+      break;
+    case 4:
+      "npqrstvwxyz";
+      console.log("D");
+      break;
+  }
+  return s;
+}
