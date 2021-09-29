a9c1c73f4bbddb85162d40f1447613328afbb8c5
Kayla Martinez <kaylamartinez1201@gmail.com>
2021-09-28 23:37:43 -0400

    saving lesson

--- HackerRank/Ten-Day-JS/conditional-statements.js
+++ HackerRank/Ten-Day-JS/conditional-statements.js
@@ -1,21 +1,25 @@
 function getLetter(s) {
-  switch (s) {
+  const target = s[0];
+  const first = "aeiou";
+  const second = "bcdfg";
+  const third = "hkjlm";
+  const fourth = "npqrstvwxyz";
+  const options = [first, second, third, fourth];
+  const targetOption = options.find((option) => option.includes(target));
+  const result = options.indexOf(targetOption);
+
+  switch (result) {
+    case 0:
+      return "A";
+      break;
     case 1:
-      "aeiou";
-      console.log("A");
+      return "B";
       break;
     case 2:
-      "bcdfg";
-      console.log("B");
+      return "C";
       break;
     case 3:
-      "hkjlm";
-      console.log("C");
-      break;
-    case 4:
-      "npqrstvwxyz";
-      console.log("D");
+      return "D";
       break;
   }
-  return s;
 }
